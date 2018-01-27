bfe9d6f934ddca69058574586035f6b64e3684e1
dzimiks <vana997@gmail.com>
2018-01-27 03:48:22 +0100

    Set main frame, need to display actors

---
+++ src/pacman/PacmanGame.java
@@ -0,0 +1,163 @@
+package pacman;
+
+import java.awt.Dimension;
+import java.awt.geom.Point2D;
+
+import pacman.view.Actor;
+import pacman.view.Game;
+
+/**
+ * @author dzimiks
+ */
+public class PacmanGame extends Game {
+
+	// maze[row][col] 
+    // 36 x 31 
+    // cols: 0-3| 4-31 | 32-35
+    public int maze[][] = {
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 3, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 3, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+    };
+    
+    public static enum State {
+    	INITIALIZING, 
+    	OL_PRESENTS, 
+    	TITLE, 
+    	READY,
+    	READY2, 
+    	PLAYING, 
+    	PACMAN_DIED,
+    	GHOST_CATCHED, 
+    	LEVEL_CLEARED,
+    	GAME_OVER
+    }
+    
+    public State state = State.INITIALIZING;
+    public int lives = 3;
+    public int score;
+    public int highscore;
+    
+    public int currentCatchedGhostScoreTableIndex = 0;
+    public final int[] catchedGhostScoreTable = {200, 400, 800, 1600};
+    public int foodCount;
+    public int currentFoodCount;
+    
+    public PacmanGame() {
+        screenSize = new Dimension(224, 288);
+        screenScale = new Point2D.Double(2, 2);
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        if (this.state != state) {
+            this.state = state;
+            broadcastMessage("stateChanged");
+        }
+    }
+    
+    public void addScore(int point) {
+        score += point;
+        
+        if (score > highscore) 
+        	highscore = score;
+    }
+    
+    public String getScore() {
+        String scoreStr = "0000000" + score;
+        scoreStr = scoreStr.substring(scoreStr.length() - 7, scoreStr.length());
+        return scoreStr;
+    }
+
+    public String getHiscore() {
+        String hiscoreStr = "0000000" + highscore;
+        hiscoreStr = hiscoreStr.substring(hiscoreStr.length() - 7, hiscoreStr.length());
+        return hiscoreStr;
+    }
+    
+    @Override
+    public void init() {
+        addAllObjs();
+        initAllObjs();
+    }
+    
+    private void addAllObjs() {
+    	
+    }
+    
+    private void initAllObjs() {
+        for (Actor actor : actors) 
+            actor.init();
+    }
+    
+    public void restoreCurrentFoodCount() {
+        currentFoodCount = foodCount;
+    }
+
+    public boolean isLevelCleared() {
+        return currentFoodCount == 0;
+    }
+    
+    public void startGame() {
+        setState(State.READY);
+    }
+    
+    public void startGhostVulnerableMode() {
+        currentCatchedGhostScoreTableIndex = 0;
+        broadcastMessage("startGhostVulnerableMode");
+    }
+    
+    public void nextLife() {
+        lives--;
+        
+        if (lives == 0) 
+            setState(State.GAME_OVER);
+        else 
+            setState(State.READY2);
+    }
+
+    public void levelCleared() {
+        setState(State.LEVEL_CLEARED);
+    }
+
+    public void nextLevel() {
+        setState(State.READY);
+    }
+
+    public void returnToTitle() {
+        lives = 3;
+        score = 0;
+        setState(State.TITLE);
+    }
+}
---
+++ src/pacman/view/Game.java
@@ -0,0 +1,68 @@
+package pacman.view;
+
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.geom.Point2D;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author dzimiks
+ */
+public class Game {
+
+	public Dimension screenSize;
+	public Point2D screenScale;
+	public List<Actor> actors = new ArrayList<>();
+	public BitmapFontRenderer bitmapFontRenderer = new BitmapFontRenderer("/res/font8x8.png", 16, 16);
+	
+	public void init() {
+		
+	}
+	
+	public void update() {
+		for (Actor actor : actors)
+			actor.update();
+	}
+	
+	public void draw(Graphics2D g) {
+		for (Actor actor : actors)
+			actor.draw(g);
+	}
+	
+	public <T> T checkCollision(Actor actor1, Class<T> type) {
+		
+		actor1.updateCollider();
+		
+		for (Actor actor2 : actors) {
+			actor2.updateCollider();
+			
+			if (actor1 != actor2 && type.isInstance(actor2) &&
+				actor1.collider != null && actor2.collider != null &&
+				actor1.visible && actor2.visible && 
+				actor2.collider.intersects(actor1.collider))
+				return type.cast(actor2);
+		}
+		
+		return null;
+	}
+	
+	public void broadcastMessage(String message) {
+		for (Actor actor : actors) {
+			try {
+				Method method = actor.getClass().getMethod(message);
+				
+				if (method != null)
+					method.invoke(actor);
+			}
+			catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+	
+	public void drawText(Graphics2D g, String text, int x, int y) {
+		bitmapFontRenderer.drawText(g, text, x, y);
+	}
+}
